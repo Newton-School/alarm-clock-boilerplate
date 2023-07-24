ab6ac5b56d5129527a9f9aa14aec49987f748162
gautamsarawagi <gautamsarawagi1@gmail.com>
2023-07-24 11:55:37 +0530

    Boileplate updated

--- app/page.js
+++ app/page.js
@@ -1,29 +1,9 @@
-import { AiOutlinePlusCircle } from 'react-icons/ai';
-import React from 'react'
-import ActiveAlarm from './ActiveAlarm';
-import AlarmContainer from './AlarmContainer';
-import AlarmForm from './AlarmForm';
 const App = () => {
 
-  const defaultAlarms = [
-    {
-        label: "Interview at abc", 
-        alarmTime: "08:30"
-    },
-    {
-        label: "Interview at xyz", 
-        alarmTime: "19:30"
-    },
-    {
-        label: "Interview at pqr", 
-        alarmTime: "20:45"
-    },
-  ]
 
   return (
     <div id="main">
-    {/*Use icon in this way*/}
-  {/*<AiOutlinePlusCircle/>*/}
+    
     </div>
   )
 }
